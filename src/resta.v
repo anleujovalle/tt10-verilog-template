d3ec3d76bc62f9d9b5373f4c08e4888dda3cdc05
Job Oscar Ernesto Anleu Ovalle <janleuo1@miumg.edu.gt>
2025-05-31 18:12:09 -0600

    Update resta.v

--- src/resta.v
+++ src/resta.v
@@ -1,11 +1,19 @@
-module RESTA (input logic [7:0] A,B, output logic [7:0] R);
-    logic [8:0] c;
-    always_comb begin
-        c[0] = 1;                           // +1 → complemento a 2
-        for (int i=0;i<8;i++) begin
-         automatic logic nB = ~B[i];
+module RESTA (
+    input  wire [7:0] A,
+    input  wire [7:0] B,
+    output reg  [7:0] R
+);
+    reg [8:0] c;
+    reg nB;
+    integer i;
+
+    always @(*) begin
+        c = 0;
+        c[0] = 1;  // complemento a 2
+        for (i = 0; i < 8; i = i + 1) begin
+            nB = ~B[i];
             R[i]   = A[i] ^ nB ^ c[i];
-            c[i+1] = (A[i]&nB) | (A[i]&c[i]) | (nB&c[i]);
+            c[i+1] = (A[i] & nB) | (A[i] & c[i]) | (nB & c[i]);
         end
     end
 endmodule
