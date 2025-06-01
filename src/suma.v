6c2626cdbcbf99f53428e4136ea80d442bda9842
Job Oscar Ernesto Anleu Ovalle <janleuo1@miumg.edu.gt>
2025-05-31 18:11:41 -0600

    Update suma.v

--- src/suma.v
+++ src/suma.v
@@ -1,10 +1,17 @@
-module suma (input logic [7:0] A,B, output logic [7:0] R);
-    logic [8:0] c;
-    always_comb begin
-        c[0] = 0;
-        for (int i=0;i<8;i++) begin
+module suma (
+    input  wire [7:0] A,
+    input  wire [7:0] B,
+    output reg  [7:0] R
+);
+    reg [8:0] c;
+    integer i;
+
+    always @(*) begin
+        c = 0;
+        for (i = 0; i < 8; i = i + 1) begin
             R[i]   = A[i] ^ B[i] ^ c[i];
-            c[i+1] = (A[i]&B[i]) | (A[i]&c[i]) | (B[i]&c[i]);
+            c[i+1] = (A[i] & B[i]) | (A[i] & c[i]) | (B[i] & c[i]);
         end
     end
 endmodule
+
