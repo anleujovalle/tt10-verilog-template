c4e4034a32dfa0a07cd9ebdbc5a01a548dd94b02
Job Oscar Ernesto Anleu Ovalle <janleuo1@miumg.edu.gt>
2025-05-31 18:14:06 -0600

    Update shiftright.v

--- src/shiftright.v
+++ src/shiftright.v
@@ -1,3 +1,6 @@
-module SHIFTRIGHT (input logic [7:0] A, output logic [7:0] R);
-    assign R = {1'b0,A[7:1]};
+module SHIFTRIGHT (
+    input  wire [7:0] A,
+    output wire [7:0] R
+);
+    assign R = {1'b0, A[7:1]};
 endmodule
