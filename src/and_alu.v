3ef9fc6a67fc8b5ea9359efe0f74098db32d42a4
Job Oscar Ernesto Anleu Ovalle <janleuo1@miumg.edu.gt>
2025-05-31 18:12:58 -0600

    Update and_alu.v

--- src/and_alu.v
+++ src/and_alu.v
@@ -1,3 +1,7 @@
-module AND_ALU (input logic [7:0] A,B, output logic [7:0] R);
+module AND_ALU (
+    input  wire [7:0] A,
+    input  wire [7:0] B,
+    output wire [7:0] R
+);
     assign R = A & B;
 endmodule
